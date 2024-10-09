361e002b22f95adac6e63504191aa1d1397f81ab
Julian MAZAT <julian.mazat12@gmail.com>
2024-10-09 17:33:57 +0200

    Passe les propriétés aux enfants

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,32 +1,43 @@
 // //TODO
 // import 'core-js'  // peut etre important (https://babeljs.io/docs/babel-preset-env#corejs)
-
+import {declare, set_context, set_context_deep} from './jsx/runtime'
 import express from 'express'
 const app = express()
 const port = 5000
 
-
-const Get = (props) => {
-  props.app.get(props.resource, props.do)
-  return null
+const Get = ({app, url, callback}) => {
+  app.get(url, callback)
 }
 
-const Routes = (props) => {
+const Resource = ({app, url, children}) => {
+  set_context(children, {url: url})
 }
 
-const App = (props) => {
-  props.app.listen(props.port, props.do)
+const Routes = ({}) => {}  // balise vide pour décoration
+
+const App = ({app, port, callback, children}) => {
+  app.listen(port, callback)  //TODO notion d'entrypoint : exécuter cette ligne à la fin (après declare)
+
+  set_context_deep(children, {app: app})
 }
 
-// TODO passer props aux enfants
 <App 
   app={app}
   port={port}
-  do = {() => console.log(`Example app listening on port ${port}`)}
+  callback = {() => console.log(`Example app listening on port ${port}`)}
 >
   <Routes>
-    <Get app={app} resource='/' do={(req, res) => {
-      res.send('Hello World!')
-    }}/>
+    <Resource url='/'>
+      <Get callback={(req, res) => {  //TODO meilleure API pr callbacks
+        res.send('Hello World!')
+      }}/>
+    </Resource>
+    <Resource url='/test'>
+      <Get callback={(req, res) => {
+        res.send('Test ok')
+      }}/>
+    </Resource>
   </Routes>
 </App>
+
+declare() //TODO doit être invisible
--- packages/api/src/jsx/runtime.js
+++ packages/api/src/jsx/runtime.js
@@ -1,12 +1,56 @@
-export const jsx = (tag, props) => {
-  let children = null
-  if(props !== null)
-    children = props.children;
-  if (typeof tag === "function")
-    return tag(props, children);
-  return null
+const declarations = [] //TODO séparer dans un module spécifique
+//TODO meilleurs noms
+
+export const jsx = (tag, props, ...children) => {
+  // permet de gérer children via props ou seul
+  if(props != null)
+    props.children = children;
+  else
+    props = {children: children}
+
+  if (typeof tag !== "function")
+    throw Error("Illegal argument: Only use JSX elements declared as functions.")
+
+  // retourne l'objet qui sera accessible par les parents du tag
+  const declaration = {
+    id: declarations.length,
+    tag: tag,
+    props: props,
+    children: children
+  }
+  declarations.push(declaration)
+  return declaration
 }
 
-export const frag = (tag, props) => {
+export const frag = (tag, props, ...children) => {
   throw Error("Not implemented: Do not use Fragments for now.")
+}
+
+export function declare() {
+  // comme les tags sont évalués de bas en haut, il faut lancer les fonctions
+  // à l'envers pour avoir une déclaration allant de haut en bas
+  for(let i = declarations.length - 1; i >= 0; i--){
+    const tag = declarations[i]
+    tag.tag(tag.props, tag.children)
+  }
+}
+
+function spread_props(child, props) {
+  Object.assign(declarations[child.id].props, props)
+}
+
+// set props for the given children
+export function set_context(children, props) {
+  for(let child of children) {
+    spread_props(child, props)
+  }
+}
+
+// recursively set props for the given children and their children...
+export function set_context_deep(children, props) {
+  set_context(children, props)
+  
+  for(let child of children)
+    if(child.children.length > 0)
+      set_context_deep(child.children, props)
 }
